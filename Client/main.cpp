4e07cc6ec855e3562441ffc14afa26010fa8ef5e
feo715 <feo715@naver.com>
2022-08-02 22:08:48 +0900

    Client for Linux

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,26 +1,30 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include <stdio.h>
-#include <iostream>
-#include <string>
-
-#include <WinSock2.h>
-#include <fstream>
 #pragma comment(lib,"ws2_32")
-#define PORT 4578
-#define PACKET_SIZE 1024
-#define SERVER_IP "192.168.200.109"
+
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>		
+#include <sys/types.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <time.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <fstream>
 
 #include "stopwatch.h"
 #include "network.h"
 #include "video.h"
 
+#define PORT 4578
+#define PACKET_SIZE 1024
 
 int BLOBSIZE(std::string weight);
-int SENDKEY(SOCKET hClient);
-int RECVKEY(SOCKET hSocket, char key[2]);
-void SENDMAT(Mat image, SOCKET hClient);
-Mat RECVMAT(SOCKET hSocket);
+int RECVKEY(char key[2], int socket);
+void SENDMAT(Mat image, int socket);
 void MOVE(char key[2]);
 
 /*
@@ -62,31 +66,30 @@ void MOVE(char key[2]);
 
 int main()
 {
+	char SERVER_IP[] = "192.168.85.153";
 	std::string name = "../data/name/coco.names";
 	std::string videofile = "../data/video/ship.mp4";
 	std::string imagefile = "../data/image/chess.jpg";
 	std::string weight = "../data/weight/coco_v5n_320.onnx";
 
-	WSADATA wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
-
-	SOCKET hSocket;
-	hSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-	SOCKADDR_IN tAddr = {};
-	tAddr.sin_family = AF_INET;
-	tAddr.sin_port = htons(PORT);
-	tAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
-
-	connect(hSocket, (SOCKADDR*)&tAddr, sizeof(tAddr)); // 지정된 소켓에 연결을 설정해준다
-
-	char cMsg[] = "Client Send";
-	send(hSocket, cMsg, strlen(cMsg), 0);
-
-	char cBuffer[PACKET_SIZE] = {};
-	recv(hSocket, cBuffer, PACKET_SIZE, 0);
-
-	printf("Recv Msg: %s\n", cBuffer);
+	int clnt_sock;
+	struct sockaddr_in serv_addr;
+	char buffer[PACKET_SIZE];
+	char message[] = "Client Send";
+	clnt_sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (clnt_sock == -1)
+		printf("socket error\n");
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+	serv_addr.sin_port = htons(PORT);
+	if (connect(clnt_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+		printf("connect error\n");
+	if (write(clnt_sock, message, sizeof(message)) == -1)
+		printf("write error\n");
+	if (read(clnt_sock, buffer, PACKET_SIZE) == -1)
+		printf("read error\n");
+	printf("Message from server :%s\n", buffer);
 
 
 	char key[2];
@@ -103,18 +106,16 @@ int main()
 	while (1)
 	{
 		capture >> image;
-		SENDMAT(image, hSocket);
+		SENDMAT(image, clnt_sock);
 		vshow(image, ClientNet);
 
-		if (RECVKEY(hSocket, key))
+		if (RECVKEY(key, clnt_sock))
 			break;
 
 		MOVE(key);
 	}
+	close(clnt_sock);
 
-	closesocket(hSocket);
-
-	WSACleanup();
 	return 0;
 }
 int BLOBSIZE(std::string weight)
@@ -149,43 +150,10 @@ int BLOBSIZE(std::string weight)
 
 	return result;
 }
-int SENDKEY(SOCKET hSocket)
-{
-	char key[2] = { 'n','n' };
-
-	if (GetAsyncKeyState('Q') & 0x8000)
-	{
-		key[0] = key[1] = 'q';
-
-		send(hSocket, key, 2, 0);
-		return 1;
-	}
-	else
-	{
-		if (GetAsyncKeyState('W') & 0x8000)
-			key[0] = 'w';
-		else if (GetAsyncKeyState('S') & 0x8000)
-			key[0] = 's';
-
-		if (GetAsyncKeyState('A') & 0x8000)
-			key[1] = 'a';
-		else if (GetAsyncKeyState('D') & 0x8000)
-			key[1] = 'd';
-
-
-		if (key[0] != 'n' || key[1] != 'n')
-		{
-			printf("%c ", key[0]);
-			printf("%c\n", key[1]);
-		}
-		send(hSocket, key, 2, 0);
-		return 0;
-	}
-}
-int RECVKEY(SOCKET hSocket, char key[2])
+int RECVKEY(char key[2], int socket)
 {
 	key[0] = key[1] = 'q';
-	recv(hSocket, key, 2, 0);
+	read(socket, key, 2);
 
 	if (key[0] == 'q')
 		return 1;
@@ -203,13 +171,13 @@ int RECVKEY(SOCKET hSocket, char key[2])
 		return 0;
 	}
 }
-void SENDMAT(Mat image, SOCKET hSocket)
+void SENDMAT(Mat image, int socket)
 {
 	char rows[4];
 	char cols[4];
 	int size = image.rows * image.cols * image.channels();
-	strcpy_s(rows, std::to_string(image.rows).c_str());
-	strcpy_s(cols, std::to_string(image.cols).c_str());
+	strcpy(rows, std::to_string(image.rows).c_str());
+	strcpy(cols, std::to_string(image.cols).c_str());
 	char* buffer = new char[size];
 
 	for (int i = 0; i < 4 - std::to_string(image.rows).size(); i++)
@@ -217,56 +185,14 @@ void SENDMAT(Mat image, SOCKET hSocket)
 	for (int i = 0; i < 4 - std::to_string(image.cols).size(); i++)
 		cols[3 - i] = -1;
 	memcpy(buffer, image.data, size);
-	send(hSocket, rows, 4, 0);
-	send(hSocket, cols, 4, 0);
-	send(hSocket, buffer, size, 0);
-	delete[]buffer;
-}
-Mat RECVMAT(SOCKET hSocket)
-{
-	char rows[4];
-	char cols[4];
-	char* buffer;
-	int size, recvbytes, riter, citer, row, col;
-	riter = citer = 1;
-	recvbytes = row = col = 0;
 
-	recv(hSocket, rows, 4, 0);
-	recv(hSocket, cols, 4, 0);
-	for (int i = 0; i < 4; i++)
-	{
-		if (rows[3 - i] != -1)
-		{
-			row += (rows[3 - i] - '0') * riter;
-			riter *= 10;
-		}
-		if (cols[3 - i] != -1)
-		{
-			col += (cols[3 - i] - '0') * citer;
-			citer *= 10;
-		}
-	}
 
-	size = row * col * 3;
-	buffer = new char[size];
-	for (int i = 0; i < size; i += recvbytes)
-	{
-		if ((recvbytes = recv(hSocket, buffer + i, row * col * 3 - i, 0)) == -1)
-			break;
-	}
-	recvbytes = 0;
-	Mat  image = Mat::zeros(row, col, CV_8UC3);
-	for (int i = 0; i < row; i++)
-	{
-		for (int j = 0; j < col; j++)
-		{
-			image.at<cv::Vec3b>(i, j) = cv::Vec3b(buffer[recvbytes + 0], buffer[recvbytes + 1], buffer[recvbytes + 2]);
-			recvbytes = recvbytes + 3;
-		}
-	}
+	//write(clnt_sock, msg, sizeof(msg));
+	write(socket, rows, 4);
+	write(socket, cols, 4);
+	write(socket, buffer, size);
 
 	delete[]buffer;
-	return image;
 }
 void MOVE(char key[2])
 {
